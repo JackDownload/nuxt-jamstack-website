5fc667f9901432c6d4f1f10079fac52e2a02927d
JackDownload <kismocsy@gmail.com>
2020-08-29 11:19:02 +0200

    sada
    dsasaasd

---
+++ pages/recipes/index.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-layout>
+    <v-flex text-xs-left>
+       <v-btn color="primary" flat nuxt to="/">Back</v-btn>
+      <h1>{{recipe.title}}</h1><br>
+            <li v-for="recipe of recipes" :key="recipe.id">
+        <n-link :to="`/recipes/${recipe.slug}`">{{ recipe.title }}</n-link>
+      </li>
+    </v-flex>
+  </v-layout>
+</template>
+<script>
+import axios from "axios";
+import config from '../../config/config';
+export default {
+    data ()
+    {recipes: []
+    },
+  asyncData (context) {
+    return axios.get(config.url + config.bucket_slug + '/object-type/recipes',{
+            params: {
+                read_key: config.read_key
+            }
+        })
+      .then(res => {
+        return {recipes : res.data.objects}
+      });
+  }
+};
+        
+</script>
